f92c42ee8cdb06a85c084dc4bf8d3338313c950b
SadithmaPerera <sadithma.20242193@iit.ac.lk>
2026-06-04 16:46:43 +0530

    Add square root calculator component with basic styling

--- src/assignments/Assignment_1.jsx
+++ src/assignments/Assignment_1.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react"
+import "./Assignment_1.css";
+
 function Assignment_1() {
-  return (
-    <div>
-      <h1>Assignment 1</h1>
-    </div>
-  )
+    const [number, setNumber] = useState(0);
+    const [sqrt, setSqrt] = useState(0);
+
+    function ButtonClick() {
+        setSqrt(Math.sqrt(number).toFixed(4));
+    }
+    return (
+        <div className="content">
+            <label>Enter the number:  </label>
+            <input type="number" style={{ width: "300px" }} onChange={(e) => setNumber(e.target.value)} />&nbsp;&nbsp;&nbsp;
+            <button onClick={ButtonClick}>Get Square Root</button>
+            <br /><br /><br />
+            <label>Square root:  </label>
+            <input type="text" style={{ enable: false, width: "300px" }} value={sqrt} />
+        </div>
+    )
 }
 
 export default Assignment_1
