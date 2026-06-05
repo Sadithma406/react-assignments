4333164d2eb4dc6e26852b124775ac2c8f4d9e5e
SadithmaPerera <sadithma.20242193@iit.ac.lk>
2026-06-05 09:58:27 +0530

    repository name was changed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/react-assignments-/',
+  base: '/react-assignments/',
   plugins: [react()],
 })
