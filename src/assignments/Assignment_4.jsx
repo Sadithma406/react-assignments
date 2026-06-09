38101f474193466b218e6edc5b698587f424359d
SadithmaPerera <sadithma.20242193@iit.ac.lk>
2026-06-09 18:50:47 +0530

    code formatting in assignment 4

--- src/assignments/Assignment_4.jsx
+++ src/assignments/Assignment_4.jsx
@@ -1,30 +1,31 @@
 import { useState } from "react";
 import './Assignment_4.css';
 
-function remove(){
-    const [num,setNum] =useState();
-    const [array,setArray] =useState([]);
-    return(
-        <div id="ass4">
-        <ul>
-            {array.map((item,index)=>
-            <li key={index}>{item} &nbsp;&nbsp;&nbsp; 
-            <button 
-            onClick={()=>setArray(array.filter((item,i)=>i!==index))}>
-                Remove
+function remove() {
+  const [num, setNum] = useState();
+  const [array, setArray] = useState([]);
+  return (
+    <div id="ass4">
+      <ul>
+        {array.map((item, index) =>
+          <li key={index}>{item} &nbsp;&nbsp;&nbsp;
+            <button
+              onClick={() => setArray(array.filter((item, i) => i !== index))}>
+              Remove
             </button>
-            
-            </li>)
-            }
-        </ul>
-        <label>Enter the number:   </label>
-            <input type="number" onChange={(e)=>setNum(e.target.value)}></input>
-                <button onClick={()=>{
-                    if(num!=null){
-                        setArray([...array,num])
-                    }
-                }}>Add</button>
-        </div>
-    )
+
+          </li>)
+        }
+      </ul>
+      <label>Enter the number:   </label>
+      <input type="number" value={num} onChange={(e) => setNum(e.target.value)}></input>
+      <button onClick={() => {
+        if (num != null) {
+          setArray([...array, num])
+          setNum("");
+        }
+      }}>Add</button>
+    </div>
+  )
 }
 export default remove;
