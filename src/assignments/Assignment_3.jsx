24dae807a5f3e13461bb670ee8e503571e1997d4
SadithmaPerera <sadithma.20242193@iit.ac.lk>
2026-06-09 17:07:30 +0530

    increasing efficiency of the code

--- src/assignments/Assignment_3.jsx
+++ src/assignments/Assignment_3.jsx
@@ -4,11 +4,14 @@ import './Assignment_3.css';
 function toggle() {
   const [num, setNum] = useState(0);
   const [array1, setArray1] = useState([]);
+  const total = array1.reduce((a, b) => Number(a) + Number(b), 0);
+  const avg = array1.length > 0 ? total / array1.length : 0;
 
   return (
     <div id="ass3">
-      <label style={{ textAlign: "left" }}>Total: {array1.reduce((a, b) => Number(a) + Number(b), 0)}</label>&nbsp;&nbsp;&nbsp;&nbsp;
-      {array1.length > 0 ? <label style={{ textAlign: "left" }}>Average: {array1.reduce((a, b) => Number(a) + Number(b), 0) / array1.length}</label> : <label style={{ textAlign: "left" }}>Average: 0</label>}
+      <label style={{ textAlign: "left" }}>Total: {total}</label>
+      &nbsp;&nbsp;&nbsp;&nbsp;
+      <label style={{ textAlign: "left" }}>Average: {avg}</label>
       <ul>
         {array1.map((item, index) => (
           <li key={index}>{item}</li>
@@ -16,8 +19,8 @@ function toggle() {
       </ul>
 
       <label>Enter the number:  </label>
-      <input type="number" onChange={(e) => setNum(e.target.value)} />&nbsp;&nbsp;
-      <button onClick={() => setArray1([...array1, num])}>Add</button><br /><br />
+      <input type="number" value={num} onChange={(e) => setNum(e.target.value)} />&nbsp;&nbsp;
+      <button onClick={() => {setArray1([...array1, num]);setNum("")}}>Add</button><br /><br />
     </div>
   )
 }
