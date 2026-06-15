0b843a16fdcf019630068ef18d1e615bb38f26e0
SadithmaPerera <sadithma.20242193@iit.ac.lk>
2026-06-15 10:43:17 +0530

    optimizing assignment 12

--- src/assignments/Assignment_12.jsx
+++ src/assignments/Assignment_12.jsx
@@ -3,27 +3,22 @@ import axios from "axios";
 import userImg from '../assets/user.jpg';
 import './Assignment_11.css';
 
+const baseURL = import.meta.env.VITE_BASE_URL;
+
+
 function Authorization() {
-  const baseURL = import.meta.env.VITE_BASE_URL;
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [msg, setMsg] = useState("");
   const [success, setSuccess] = useState(false);
   const [data, setData] = useState(null);
-
+  const [status, setStatus] = useState("pending");
   const [checked, setChecked] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  useEffect(() => {
-    const token = localStorage.getItem("token") || sessionStorage.getItem("token");
-    if (token) {
-      setIsLoggedIn(true);
-      getToken(token);
-    }
-  }, [])
-
-  function getToken(method) {
-    axios.get(`${baseURL}/user`, { headers: { Authorization: `Bearer ${method}` } }).then(
+  const getToken = () => localStorage.getItem("token") || sessionStorage.getItem("token")
+  const getHeading = () => ({ Authorization: `Bearer ${getToken()}` })
+  function loadUser(method) {
+    axios.get(`${baseURL}/user`, { headers: getHeading() }).then(
       response => {
         console.log(response.data);
         setData(response.data);
@@ -34,8 +29,8 @@ function Authorization() {
       }
     )
   }
-  async function handleLogin() {
 
+  async function handleLogin() {
     try {
       const response = await axios.post(`${baseURL}/login`, {
         email, password
@@ -44,13 +39,12 @@ function Authorization() {
       const token = response.data.access_token;
       if (checked) {
         localStorage.setItem("token", token);
-        getToken(localStorage.getItem("token"));
       }
       else {
         sessionStorage.setItem("token", token);
-        getToken(sessionStorage.getItem("token"));
       }
-      setIsLoggedIn(true);
+      loadUser(token);
+      setStatus("loggedIn")
       setMsg("Successfully logged in");
       setSuccess(true);
     }
@@ -61,31 +55,42 @@ function Authorization() {
       setData(null);
     }
   }
+
+  useEffect(() => {
+
+    setStatus(getToken() === null ? "login" : "loggedIn");
+    loadUser();
+
+  }, [])
+
+  if (status === "pending") {
+    return <p>Loading...</p>
+  }
   return (
     <div id="ass11">
-        <div style={{display: isLoggedIn?"none":"block"}}>
-          <label>Enter the Email:  </label>
-          <input type="text" value={email} onChange={(e) => setEmail(e.target.value)}></input>
-          <br /><br />
-          <label>Enter the password:  </label>
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
-          <br /><br />
-          <label>Keep me logged in: </label>
-          <input type="checkbox" checked={checked} onChange={() => setChecked(!checked)}></input>
-          <br /><br />
-          <button onClick={handleLogin}>Login</button>
-          <p style={{ color: success ? "green" : "red" }}>{msg}</p>
-          <br /><br />
-        </div>
-        <div>
-          {data && <div className="user-data">
-            <img src={data.avatar ? data.avatar : userImg} alt="avatar" width="120" height="120"></img><br />
-            <p>Name: {data.name}</p>
-            <p>Email: {data.email}</p>
-            <p>Description: {data.description ? data.description : "-"}</p>
-            <p>Subscribed? : {data.subscribed === true ? "Yes" : "No"}</p>
-          </div>}
-        </div>
+      <div style={{ display: status == "loggedIn" ? "none" : "block" }}>
+        <label>Enter the Email:  </label>
+        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)}></input>
+        <br /><br />
+        <label>Enter the password:  </label>
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
+        <br /><br />
+        <label>Keep me logged in: </label>
+        <input type="checkbox" checked={checked} onChange={() => setChecked(!checked)}></input>
+        <br /><br />
+        <button onClick={handleLogin}>Login</button>
+        <p style={{ color: success ? "green" : "red" }}>{msg}</p>
+        <br /><br />
+      </div>
+      <div>
+        {data && <div className="user-data">
+          <img src={data.avatar ? data.avatar : userImg} alt="avatar" width="120" height="120"></img><br />
+          <p>Name: {data.name}</p>
+          <p>Email: {data.email}</p>
+          <p>Description: {data.description ? data.description : "-"}</p>
+          <p>Subscribed? : {data.subscribed === true ? "Yes" : "No"}</p>
+        </div>}
+      </div>
     </div>
   )
 }
